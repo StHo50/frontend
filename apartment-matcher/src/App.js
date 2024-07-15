bdf5137e5c272bed4a1a09aefd059622fa89308c
HannaSofer <hannasofer1410@gmail.com>
2024-07-15 22:38:30 +0300

    Added a comment - sanity check

--- apartment-matcher/src/App.js
+++ apartment-matcher/src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import About from './components/About/About';
-import Matching from './components/Apartment-Matching/Matching';
-import CustomerReviewsPage from './components/Customer-Reviews/CustomerReviewsPage';
-import Contact from './components/Contact/Contact';
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
-import Home from './components/Homepage/Home';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import About from "./components/About/About";
+import Matching from "./components/Apartment-Matching/Matching";
+import CustomerReviewsPage from "./components/Customer-Reviews/CustomerReviewsPage";
+import Contact from "./components/Contact/Contact";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Home from "./components/Homepage/Home";
+import "./App.css";
+
+// Comment
 
 function App() {
   return (
@@ -18,7 +20,10 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/matching" element={<Matching />} />
-          <Route path="/customerreviewspage" element={<CustomerReviewsPage />} />
+          <Route
+            path="/customerreviewspage"
+            element={<CustomerReviewsPage />}
+          />
           <Route path="/contact" element={<Contact />} />
         </Routes>
       </main>
